214e84a8194c1abdb91b096877d3be049e05289e
bjb298 <bjb298@byu.edu>
2023-05-09 17:23:54 -0600

    added postgresql

--- cessoc/postgresql.py
+++ cessoc/postgresql.py
@@ -1,1 +1,35 @@
+import psycopg2 as pg
+
 print("postgresql imported")
+
+
+class postgresql:
+    def __init__(self, host, database, user, password, port=5432):
+        self.connection = pg.connect(
+            host=host, database=database, user=user, password=password, port=port)
+
+    def create(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
+        cursor.close()
+        
+    def read(self, query) -> list:
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+    def update(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
+        cursor.close()
+
+    def delete(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
+        cursor.close()
+        
