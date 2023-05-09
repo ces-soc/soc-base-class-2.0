7ed4b97b230e3909321eb418e9684aff484a9943
bjb298 <bjb298@byu.edu>
2023-05-09 17:25:08 -0600

    removed main

--- main.py
+++
@@ -1,8 +0,0 @@
-from cessoc import humio
-from cessoc.rabbitmq import rabbitmq
-
-humio.write(data=[{"test":"test"}], endpoint="https://secops-humio-dev.byu.edu/api/v1/ingest/humio-unstructured", token="57c1a7b7-086c-432b-96f0-960636ece63e")
-
-rabbitmq.hi
-
-print("finished")
