2038ad0b835f470c2c9b2cd8243310299acfc6df
bjb298 <bjb298@byu.edu>
2023-07-18 17:37:33 -0600

    modified healthcheck to be called automatically, added crash reporting

--- cessoc/healthcheck.py
+++ cessoc/healthcheck.py
@@ -8,6 +8,8 @@ import os
 from typing import Optional, Union
 from cessoc import humio
 from cessoc.aws import ssm
+import atexit
+import sys
 
 
 class HealthCheck:
@@ -25,19 +27,38 @@ class HealthCheck:
         self.service_name = service_name
         self.campus = os.environ["CAMPUS"].lower()
         self.timezone = tzlocal.get_localzone()
+        atexit.register(self.end)
 
-    def send(self, custom_data: Union[str, dict] = "None", endpoint: Optional[str] = None, token: Optional[str] = None):
+    def end(self):
         """
-        Sends the healthcheck data to humio.
+        Sends healthcheckt data to Humio after the service has ended.
+        """
+        try: # gets the last uncaught exception see https://docs.python.org/3/library/sys.html#sys.exc_info
+            error = str(sys.last_type) + str(sys.last_value)
+            # error = sys.last_traceback
+        except AttributeError:
+            error = None
+        if error is None:
+            self.send(status="completed")
+        else:
+            self.send(status="errored", custom_data={"error": error})
+        print("exiting...")
+
+    def send(self, custom_data: Union[str, dict] = "None", endpoint: Optional[str] = None, token: Optional[str] = None, status = "running"):
+        """
+        Sends the healthcheck data to humio. This will run automatically when the program exits. This can be called on a long running service to send periodic healthcheck data.
 
         :param custom_data: The custom data to be sent to humio. Must be a json object
         :param token: The humio ingest token
         :param endpoint: The humio ingest endpoint
+        :param status: The status of the service. Defaults to "running". Can be "running", "errored", or "completed"
         """
         if endpoint is None:
             endpoint = ssm.get_value(f"/{self.campus}/secops-humio/config/api_endpoint") + "ingest/humio-unstructured"
         if token is None:
             token = ssm.get_value(f"/{self.campus}/secops-humio/secrets/healthcheck/ingest_token")
+        if status not in ["running", "errored", "completed"]: # check if status is valid
+            raise ValueError("status must be 'running', 'errored', or 'completed'")
         # set end time
         self.end_time = time.time()
         # Make the runtime easily readable
@@ -49,6 +70,7 @@ class HealthCheck:
             "start_time": f"{datetime.fromtimestamp(self.start_time, tz=self.timezone)}",
             "end_time": f"{datetime.fromtimestamp(self.end_time, tz=self.timezone)}", 
             "service_name": f"{self.service_name}",
+            "status": f"{status}",
             "data": custom_data,
             "runtime": f"{readable_runtime}",
             "@timestamp": f"{self.end_time}"
