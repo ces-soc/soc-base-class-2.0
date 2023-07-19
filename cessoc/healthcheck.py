b28b97364fe6b59ac1ca5eed909ddde09f3e3f0e
Brian Byrd <101357417+bjb298@users.noreply.github.com>
2023-07-18 18:01:31 -0600

    Dev (#24)
    * All the __init__.py
    
    * First commit (#1)
    
    * first commit-- s3, ssm, util, humio, eventhub
    
    * added healthcheck and rabbit stuff
    
    * added postgresql
    
    * removed main
    
    * added pyproject toml (#2)
    
    * removed logging stuff in rabbit
    
    * removed more logger instances
    
    * update
    
    * find and replaced logger with logging
    
    * removed another custom logger
    
    * added pytest, changed some naming, fixed a few bugs
    
    * added github actions tests
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * pylint
    
    * updated base class based on feedback
    
    * updated based on feedback
    
    * removed spaces for pylint
    
    * updated pytests to reflect changes
    
    * made path optional, humio deaults to parameter store entry
    
    * error handling
    
    * errror handling in heallthcheck
    
    * errror handling in heallthcheck (#17)
    
    * fixed rabbit's default parameters bug
    
    * removed publish_message_campus and added an optional variable to publish_message
    
    * modified healthcheck to be called automatically, added crash reporting
    
    * naming convention so end is not called by users
    
    * added log when healthcheck is run
    
    * flake8
    
    ---------
    
    Co-authored-by: Tyler Johnson <tjohn351@gmail.com>

--- cessoc/healthcheck.py
+++ cessoc/healthcheck.py
@@ -8,6 +8,9 @@ import os
 from typing import Optional, Union
 from cessoc import humio
 from cessoc.aws import ssm
+import atexit
+import sys
+import logging
 
 
 class HealthCheck:
@@ -25,19 +28,36 @@ class HealthCheck:
         self.service_name = service_name
         self.campus = os.environ["CAMPUS"].lower()
         self.timezone = tzlocal.get_localzone()
+        atexit.register(self._end)
 
-    def send(self, custom_data: Union[str, dict] = "None", endpoint: Optional[str] = None, token: Optional[str] = None):
+    def _end(self):
         """
-        Sends the healthcheck data to humio.
+        Sends healthcheckt data to Humio after the service has ended. Or crashed.
+        """
+        try: # gets the last uncaught exception see https://docs.python.org/3/library/sys.html#sys.exc_info
+            error = str(sys.last_type) + str(sys.last_value)
+        except AttributeError:
+            error = None
+        if error is None:
+            self.send(status="completed")
+        else:
+            self.send(status="errored", custom_data={"error": error})
+
+    def send(self, custom_data: Union[str, dict] = "None", endpoint: Optional[str] = None, token: Optional[str] = None, status="running"):
+        """
+        Sends the healthcheck data to humio. This will run automatically when the program exits. This can be called on a long running service to send periodic healthcheck data.
 
         :param custom_data: The custom data to be sent to humio. Must be a json object
         :param token: The humio ingest token
         :param endpoint: The humio ingest endpoint
+        :param status: The status of the service. Defaults to "running". Can be "running", "errored", or "completed"
         """
         if endpoint is None:
             endpoint = ssm.get_value(f"/{self.campus}/secops-humio/config/api_endpoint") + "ingest/humio-unstructured"
         if token is None:
             token = ssm.get_value(f"/{self.campus}/secops-humio/secrets/healthcheck/ingest_token")
+        if status not in ["running", "errored", "completed"]: # check if status is valid
+            raise ValueError("status must be 'running', 'errored', or 'completed'")
         # set end time
         self.end_time = time.time()
         # Make the runtime easily readable
@@ -49,6 +69,7 @@ class HealthCheck:
             "start_time": f"{datetime.fromtimestamp(self.start_time, tz=self.timezone)}",
             "end_time": f"{datetime.fromtimestamp(self.end_time, tz=self.timezone)}", 
             "service_name": f"{self.service_name}",
+            "status": f"{status}",
             "data": custom_data,
             "runtime": f"{readable_runtime}",
             "@timestamp": f"{self.end_time}"
@@ -58,4 +79,5 @@ class HealthCheck:
         if os.getenv("STAGE") is not None:
             healthdata[0]['env'] = os.getenv("STAGE")
 
+        logging.log("sending healthcheck data to humio")
         humio.write(data=healthdata, endpoint=endpoint, token=token, path="healthcheck")
