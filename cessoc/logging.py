ddf87dc119528c7c31c3492e6eac14e4d371c426
Jonathan Black <jblack@byu.edu>
2023-08-21 17:17:39 -0600

    Add logging

---
+++ cessoc/logging.py
@@ -0,0 +1,60 @@
+import logging
+import os
+from pythonjsonlogger import jsonlogger
+from logging.handlers import RotatingFileHandler
+import sys
+
+class soc_logging:
+    logging_setup = False
+    logger = None
+    def set_root_logging(force_json_logging=False, file_log=False):
+        """
+        Sets up the root logger so it will properly output data.
+        """
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.INFO)
+        handler = soc_logging.get_logging_handler(force_json_logging, file_log)
+        if len(root_logger.handlers) != 0:
+            root_logger.handlers = []
+        root_logger.addHandler(handler)
+        # set log format
+        if soc_logging.isatty() and force_json_logging is False:
+            # if terminal, set color output
+            handler.setFormatter(
+                logging.Formatter(
+                    "[%(asctime)s] [%(levelname)-19s] [%(lineno)04d] [%(name)-25s] [%(funcName)-15s] %(message)s"
+                )
+            )
+        else:
+            # if non-terminal, set json output
+            handler.setFormatter(
+                logging.jsonlogger.JsonFormatter(
+                    "%(asctime)s %(levelname)s %(lineno)d %(name)s %(funcName)s %(message)s"
+                )
+            )
+        soc_logging.logging_setup = True
+    def get_logging_handler(force_json_logging, file_log):
+        """Gets the corresponding logging handler based on the type configured for the class. Can be a stdout or file logger"""
+        if file_log:
+            # if logging to a file, do so in json
+            force_json_logging = True
+            # Create the rotating file handler. Limit the size to 1000000Bytes ~ 1MB .
+            return RotatingFileHandler("log.json", maxBytes=1000000, backupCount=2)
+        else:
+            # set logging to stdout
+            return logging.StreamHandler(sys.stdout)
+    def isatty() -> bool:
+        """
+        Check if stdout is going to a terminal
+        :returns: True or False if current tty is a terminal
+        """
+        if (hasattr(sys.stdout, "isatty") and sys.stdout.isatty()) or (
+            "TERM" in os.environ and os.environ["TERM"] == "ANSI"
+        ):
+            return True
+        return False
+    def getLogger(name, force_json_logging=False, file_log=False):
+        if not soc_logging.logging_setup:
+            soc_logging.set_root_logging(force_json_logging=force_json_logging, file_log=file_log)
+            soc_logging.logger = logging.getLogger(name)     
+        
