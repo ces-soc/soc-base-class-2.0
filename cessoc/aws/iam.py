1b23dd37713011f54388ee5f136734730d29f1c7
Jonathan Black <jblack@byu.edu>
2023-10-18 16:09:57 -0600

    RDSGetToken Function

--- cessoc/aws/iam.py
+++ cessoc/aws/iam.py
@@ -6,11 +6,10 @@ from typing import Optional
 from cessoc.aws import ssm
 
 
-class iam:
-    def getRDSToken(DBUsername: str, DBHostname: Optional[str] = None, Port: Optional[int] = 5432, Region: Optional[str] = "us-west-2") -> str:
-        """Gets an IAM token"""
-        session = boto3.Session()
-        client = session.client('rds')
-        if DBHostname is None:
-            DBHostname = ssm.get_value("/ces/data_store/rds_host")
-        return client.generate_db_auth_token(DBHostname=DBHostname, Port=Port, DBUsername=DBUsername, Region=Region)
+def getRDSToken(DBUsername: str, DBHostname: Optional[str] = None, Port: Optional[int] = 5432, Region: Optional[str] = "us-west-2") -> str:
+    """Gets an IAM token"""
+    session = boto3.Session()
+    client = session.client('rds')
+    if DBHostname is None:
+        DBHostname = ssm.get_value("/ces/data_store/rds_host")
+    return client.generate_db_auth_token(DBHostname=DBHostname, Port=Port, DBUsername=DBUsername, Region=Region)
